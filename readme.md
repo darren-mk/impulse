0002eaa64397b807d2d81fb9f0676417e1bb26e3
Darren Kim <darren.minsoo.kim@gmail.com>
2025-11-02 11:59:23 -0500

    apply html template and datastar

--- readme.md
+++ readme.md
@@ -6,3 +6,15 @@
 - axum
 - datastar
 - sqlx on postgres
+
+### Get Started
+
+- development
+```shell
+cargo watch -x run
+```
+
+- build
+```shell
+cargo build
+```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use axum::{routing::get, Router};
+use askama::Template;
+use axum::{response::Html, routing::get, Router};
 
 const PORT_NUM: &str = "0.0.0.0:3000";
 
 #[tokio::main]
 async fn main() {
     let app: axum::Router<_> = 
-        Router::new().route("/", get(|| async {"Hello World!"}));
+        Router::new().route("/", get(handler));
     let listener: tokio::net::TcpListener =
         tokio::net::TcpListener::bind(PORT_NUM).await.unwrap();
     axum::serve(listener, app).await.unwrap();}
+
+async fn handler() -> Html<String> {
+    Html(IndexTemplate { body: "<h1>Hello</h1>" }.render().unwrap())}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate<'a> { body: &'a str, }
