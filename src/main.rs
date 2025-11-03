e299972e0daca6fc9ae84479d8383df9da31255e
Darren Kim <darren.minsoo.kim@gmail.com>
2025-11-02 19:00:06 -0500

    revoke html template approach

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use askama::Template;
 use axum::{response::Html, routing::get, Router};
 
 const PORT_NUM: &str = "0.0.0.0:3000";
@@ -6,14 +5,7 @@ const PORT_NUM: &str = "0.0.0.0:3000";
 #[tokio::main]
 async fn main() {
     let app: axum::Router<_> = 
-        Router::new().route("/", get(handler));
+        Router::new().route("/", get(|| async { Html("Hello, World!") }));
     let listener: tokio::net::TcpListener =
         tokio::net::TcpListener::bind(PORT_NUM).await.unwrap();
     axum::serve(listener, app).await.unwrap();}
-
-async fn handler() -> Html<String> {
-    Html(IndexTemplate { body: "<h1>Hello</h1>" }.render().unwrap())}
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate<'a> { body: &'a str, }
